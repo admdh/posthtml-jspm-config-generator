0a57b5b0d585bb4967ac597fb34f879ae3fdfd34
Eduárd Pocstár <eduard.pocstar@admdh.com>
2016-06-07 11:05:44 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+module.exports = function (options) {
+	opts = options || {
+			outputPath: './src/jspm-imports.js'
+		};
+
+	return function posthtmlModularCss(tree){
+		var newFileContent = ['\'use strict\'\;'];
+
+		tree.match({ tag: 'script' }, function(node) {
+			if (typeof node.content !== 'undefined'){
+				var rawContent = node.content;
+				var strContent = rawContent.toString();
+
+				if (strContent.indexOf('System.import') >= 0 ) {
+					var fileUrl = strContent.match(/\"([^()]+)\"|\'([^()]+)\'/g);
+					var jspmConfUrl = 'import ' + fileUrl.toString() + '';
+				}
+				newFileContent.push(jspmConfUrl)
+			}
+		});
+
+		fs.writeFile(opts.outputPath, newFileContent.join('\n'), function (err) {
+			if (err){
+				return console.log(err);
+			}
+			console.log('jspm config file created!');
+		});
+	};
+};
